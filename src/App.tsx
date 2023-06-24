cffe6e24a0cad2b3998f9a17e5e724a6dd176965
Erick Arcos <arcosea@plu.edu>
2023-06-23 19:16:09 -0700

    feature: back button to show full cam

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import puzzle from "./assets/puzzle.png";
+import piece from "./assets/piece.png";
+import back from "./assets/back.png";
 
 function App() {
   const fullPuzzlePhotoRef = useRef<any>(null);
+  const piecePhotoRef = useRef<any>(null);
   const fullPuzzleVideoRef = useRef<any>(null);
-  const [photoTaken, setPhotoTaken] = useState<boolean>(false);
+  const [puzzlePhotoTaken, setPuzzlePhotoTaken] = useState<boolean>(false);
+  const [piecePhotoTaken, setPiecePhotoTaken] = useState<boolean>(false);
+  const [buttonImage, setButtonImage] = useState<any>(puzzle);
 
   // Display camera
   const getVideo = () => {
     navigator.mediaDevices
       .getUserMedia({
         video: {
-          // width: { min: 120, ideal: 100, max: 1920 },
-          // height: { min: 120, ideal: cameraHeight, max: 1080 },
+          width: { min: 120, ideal: 100, max: 1920 },
+          height: { min: 120, ideal: 100, max: 1080 },
           facingMode: "environment",
         },
       })
@@ -52,25 +58,39 @@ function App() {
     let image = new Image();
     image.src = dataUrl;
 
-    setPhotoTaken(true);
+    setPuzzlePhotoTaken(true);
+    setButtonImage(piece);
+  };
+
+  const revertBack = () => {
+    setPuzzlePhotoTaken(false);
+    getVideo();
   };
 
   return (
     <>
       <canvas
         id="fullPuzzlePhoto"
-        className={photoTaken ? "" : "hide"}
+        className={puzzlePhotoTaken ? "" : "hide"}
         ref={fullPuzzlePhotoRef}
       ></canvas>
       <video
         id="fullPuzzleVideo"
-        className={photoTaken ? "hide" : ""}
+        className={puzzlePhotoTaken ? "hide" : ""}
         ref={fullPuzzleVideoRef}
       ></video>
 
       <div id="spacePlacer"></div>
       <button id="fullPuzzleButton" onClick={takePhoto}>
-        {" "}
+        <input id="buttonImage" type="image" src={buttonImage} />
+      </button>
+
+      <button
+        id="fullPuzzleButton"
+        className={puzzlePhotoTaken ? "" : "hide"}
+        onClick={revertBack}
+      >
+        <input id="buttonImage" type="image" src={back} />
       </button>
     </>
   );
